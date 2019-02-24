ba27a700d400543bc2cef1c9d7f26fcfceb849df
Carbon <juan.carbo94@gmail.com>
2019-02-24 05:52:58 -0300

    css bugfixing

--- src/js/Contact.js
+++ src/js/Contact.js
@@ -2,23 +2,23 @@ import React, { Component } from 'react'
 
 export default class Contact extends Component {
     render() {
-        
+
         const linkedIn = require('../img/linkedin.svg')
         const gmail = require('../img/gmail.svg')
         return (
             <div className='container-fluid p-5 bigger-text'>
-            <h1 className='display-1 font-weight-bold'>Contact Information</h1>
+                <h1 className='display-1 font-weight-bold'>Contact Information</h1>
                 <br />
                 <div className='transparent-box'>
 
                     <div onClick={() => window.open('mailto:juan.carbo94@gmail.com', '_blank')}>
-                        <img src={gmail} alt="Gmail" style={{ display: "block", height: '50px', width: '50px' }} />
-                        <h3>Juan.Carbo94@gmail.com</h3>
+                        <img src={gmail} alt="Gmail" className='contactIcon' />
+                        <h3 className='contactText'>Juan.Carbo94@gmail.com</h3>
                     </div>
                     <div onClick={() => window.open('www.linkedin.com/in/juan-carbo', '_blank')}>
-                        <img src={linkedIn} alt="LinkedIn" style={{ display: "block", height: '50px', width: '50px' }}></img>
+                        <img src={linkedIn} alt="LinkedIn" className='contactIcon'></img>
 
-                        <h3>www.linkedin.com/in/juan-carbo</h3>
+                        <h3 className='contactText'>www.linkedin.com/in/juan-carbo</h3>
                     </div>
                 </div>
 
